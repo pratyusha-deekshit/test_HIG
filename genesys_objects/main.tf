bdfb9aa2bc0dad55459c7029aebaf45f52664e6a
pratyusha-deekshit <126081699+pratyusha-deekshit@users.noreply.github.com>
2023-08-08 18:29:30 +0530

    Update main.tf

--- genesys_objects/main.tf
+++ genesys_objects/main.tf
@@ -25,3 +25,4 @@ module "users" {
 resource "genesyscloud_flow" "mysimpleflow" {
   filepath = "./modules/call_flow/new_v1-0.yaml"
   file_content_hash = filesha256("./modules/call_flow/new_v1-0.yaml") 
+}

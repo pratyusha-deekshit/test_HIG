12c63274b6292d2ca26a524efc80ea8c7371c9ac
pratyusha-deekshit <126081699+pratyusha-deekshit@users.noreply.github.com>
2023-08-22 17:24:18 +0530

    Update main.tf

--- genesys_objects/main.tf
+++ genesys_objects/main.tf
@@ -22,7 +22,6 @@ module "auth_division" {
   source = "./modules/auth_division"   
 }
 
-/*
 # Module for routing-queue
 module "routing_queue" {
     source = "./modules/routing_queue"
@@ -43,4 +42,3 @@ resource "genesyscloud_flow" "flowname" {
   filepath = "./modules/workflows/SimpleFinancialIvr_v2-0.yaml"
   file_content_hash = filesha256("./modules/workflows/SimpleFinancialIvr_v2-0.yaml") 
 }
-*/
